2d79b673890076f2f599f87f52b9d490f92ad7fe
pizzaogkebab <j.kwaniewski0813@gmail.com>
2022-12-03 00:08:49 +0100

    Day 1. solution

--- 1.py
+++ 1.py
@@ -1,13 +1,13 @@
-def compute(records: list) -> list:
+def list_of_cal(records: list) -> list:
     sums = []
     for x in records:
         sums.append(sum(map(int, x)))
     return sorted(sums, reverse= True)
 
 
-with open('1.txt' , 'r') as input_s:
-    s = input_s.read()
-    data = list(part.splitlines() for part in s.split('\n\n'))
+with open('1.txt', 'r') as input_str:
+    s = input_str.read()
+    data = list(element.splitlines() for element in s.split('\n\n'))
 
-print(f"Part 1 answer: {compute(data)[0]}")
-print(f"Part 2 answer: {sum(compute(data)[0:3])}")
+print(f"Part 1 answer: {list_of_cal(data)[0]}")
+print(f"Part 2 answer: {sum(list_of_cal(data)[0:3])}")
