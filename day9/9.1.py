6ef18d6b038af73499a08085b37730b829a4c791
pizzaogkebab <j.kwasniewski0813@gmail.com>
2022-12-10 00:49:04 +0100

    Day 9. first part solution

--- day9/9.1.py
+++ day9/9.1.py
@@ -1,64 +1,49 @@
-with open('9.txt', 'r') as input_str:
-    data = input_str.read()
-
-def compute(records):
+def positions_quantity(records:str) -> int:
     h = [0, 0]
     t = [0, 0]
-    positions = {(0, 0)}
+    unique = {(0, 0)}
     for line in records.splitlines():
         line = line.split()
-        direction, = line[0]
-        number = int(line[1])
+        direction, number = line[0], int(line[1])
+
+        # left
         if direction == 'L':
             h[0] -= number
-            if abs(h[0] - t[0]) >= 2 or abs(h[1] - t[1]) >= 2:
-                if h[1] == t[1]:
-                    for i in range(h[0], t[0] - 1):
-                        t[0] -= 1
-                        positions.add(tuple(t))
-                else:
-                    t[1] = h[1]
-                    for i in range(h[0], t[0] - 1):
-                        t[0] -= 1
-                        positions.add(tuple(t))
+            if abs(h[0] - t[0]) >= 2 or abs(h[1] - t[1]) >= 2:  # check if move is necessary
+                t[1] = h[1]  # setting on proper height or width
+                for i in range(h[0], t[0] - 1):
+                    t[0] -= 1
+                    unique.add(tuple(t))
+
+        # right
         elif direction == 'R':
             h[0] += number
-            if abs(h[0] - t[0]) >=2 or abs(h[1] - t[1]) >=2:
-                if h[1] == t[1]:
-                    for i in range(t[0], h[0] - 1):
-                        t[0] += 1
-                        positions.add(tuple(t))
-                else:
-                    t[1] = h[1]
-                    for i in range(t[0], h[0] - 1):
-                        t[0] +=1
-                        positions.add(tuple(t))
+            if abs(h[0] - t[0]) >= 2 or abs(h[1] - t[1]) >= 2:
+                t[1] = h[1]
+                for i in range(t[0], h[0] - 1):
+                    t[0] += 1
+                    unique.add(tuple(t))
 
+        # down
         elif direction == 'D':
             h[1] -= number
-            if abs(h[0] - t[0]) >=2 or abs(h[1] - t[1]) >=2:
-                if h[0] == t[0]:
-                    for i in range(h[1], t[1] - 1):
-                        t[1] -= 1
-                        positions.add(tuple(t))
-                else:
-                    t[0] = h[0]
-                    for i in range(h[1], t[1] - 1):
-                        t[1] -=1
-                        positions.add(tuple(t))
+            if abs(h[0] - t[0]) >= 2 or abs(h[1] - t[1]) >= 2:
+                t[0] = h[0]
+                for i in range(h[1], t[1] - 1):
+                    t[1] -= 1
+                    unique.add(tuple(t))
+
+        # up
         elif direction == 'U':
             h[1] += number
-            if abs(h[0] - t[0]) >=2 or abs(h[1] - t[1]) >=2:
-                if h[0] == t[0]:
-                    for i in range(t[1], h[1] - 1):
-                        t[1] += 1
-                        positions.add(tuple(t))
-                else:
-                    t[0] = h[0]
-                    for i in range(t[1], h[1] - 1):
-                        t[1] += 1
-                        positions.add(tuple(t))
-        print(h, t)
-    print(positions)
-    print(len(positions))
-compute(data)
+            if abs(h[0] - t[0]) >= 2 or abs(h[1] - t[1]) >= 2:
+                t[0] = h[0]
+                for i in range(t[1], h[1] - 1):
+                    t[1] += 1
+                    unique.add(tuple(t))
+    return len(unique)
+
+
+with open('9.txt', 'r') as input_str:
+    data = input_str.read()
+print(f"part 1. solution: {positions_quantity(data)}")
