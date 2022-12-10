5838194f21c543fe537f73ba29e8bf0e41fc717a
pizzaogkebab <j.kwasniewski0813@gmail.com>
2022-12-10 14:08:32 +0100

    Day 10. first prat solution

---
+++ day10/10.1.py
@@ -0,0 +1,21 @@
+def compute(records: str):
+    cycle = 0
+    x = 1
+    sum_of_xs = 0
+    for line in records.splitlines():
+        instr, *value = line.strip().split()
+        cycle += 1
+        if (cycle - 20) % 40 == 0:
+            sum_of_xs += x * cycle
+        if instr == "addx":
+            cycle += 1
+            if (cycle - 20) % 40 == 0:
+                sum_of_xs += x * cycle
+            x += int(*value)
+    return sum_of_xs
+
+
+with open('10.txt', 'r') as input_str:
+    data = input_str.read()
+
+print(compute(data))
