ddb30832c49b7a59240476397ec5aa33d9b5357e
pizzaogkebab <j.kwasniewski0813@gmail.com>
2022-12-15 14:10:26 +0100

    Day 13. first part solution

--- day13/13.1.py
+++ day13/13.1.py
@@ -1,2 +1,27 @@
 with open('13.txt', 'r') as input_str:
     data = input_str.read()
+
+def compute(x, y) -> int:
+    if type(x) == int:
+        if type(y) == int:
+            return x - y
+        else:
+            return compute([x], y)
+    else:
+        if type(y) == int:
+            return compute(x, [y])
+
+    for a,b in zip(x,y):
+        value = compute(a,b)
+        if value:
+            return value
+
+    return len(x) - len(y)
+
+def pairs_count(records: str) -> int:
+    records = [x.split() for x in record.split('\n\n')]
+    for ind, (first, second) in enumerate(records, start=1):
+        if compute(eval(first),eval(second)) < 0:
+            pairs += ind
+    return pairs
+print(f"first part answer: {pairs_count(data)}")
